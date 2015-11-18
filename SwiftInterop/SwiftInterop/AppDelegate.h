0c2017569fb64c88a4cd93c545262d275e5485c7
wesleystjohn <wesley.stjohn@mobileforming.com>
2015-11-17 21:29:01 -0800

    first commit

---
+++ SwiftInterop/SwiftInterop/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SwiftInterop
+//
+//  Created by Wesley St. John on 11/17/15.
+//  Copyright © 2015 Mobileforming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
